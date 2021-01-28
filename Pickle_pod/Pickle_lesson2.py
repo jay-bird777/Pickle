365c20bfa72cb7cc6eef2b99b326495f13182a8a
jay-bird777 <67336371+jay-bird777@users.noreply.github.com>
2021-01-28 05:39:11 +0300

    idk lol

---
+++ Pickle_pod/Pickle_lesson2.py
@@ -0,0 +1,21 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: /Users/jaybird/Documents/idk.py ===================
+This POD Leader is Jacore
+Jacore Baptiste (845) 200-6250
+Moussa Ndiaye (123) 456-7890
+Morris Jones (925) 286-5922
+Prince Fields (510) 472-0804
+Akari Johnson (510) 500-2206
+
+
+This POD Leader is Andrew
+Andrew Lubega (925) 727-4611
+Mallick Abdullah (510) 409-8755
+Ronin Youngjones (415) 910-3415
+Glenn Ivory (510) 328-8290
+
+
+>>> 
